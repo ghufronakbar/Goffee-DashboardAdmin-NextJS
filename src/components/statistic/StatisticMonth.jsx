16d0ff2938ce7242d93c2afb372ad5d8f417e20c
ghufron akbar <alan.040219.gam@gmail.com>
2024-05-08 23:43:31 +0700

    Chart JS

---
+++ src/components/statistic/StatisticMonth.jsx
@@ -0,0 +1,80 @@
+import {
+  Box,
+  Text,
+  Stack,
+  Tr,
+  Table,
+  Th,
+  Td,
+} from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "@/lib/axios";
+import { LoadingComponent } from "../LoadingComponent";
+
+export default function StatisticMonth({ flex }) {
+  const {
+    data: dataMenu,
+    isLoading,
+    error,
+    refetch: refetchDataMenu,
+  } = useQuery({
+    queryKey: ["order/total/income/month"],
+    queryFn: async () => {
+      const dataResponse = await axiosInstance.get("/order/total/income/month");
+      return dataResponse;
+    },
+  });
+
+  if (isLoading) return <LoadingComponent flex={flex} />;
+
+  if (error) {
+    return (
+      <Box
+        flex={flex}
+        p={8}
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        mt={4}
+        mx={4}
+        
+      >
+        <Text>Error fetching data</Text>
+      </Box>
+    );
+  }
+
+  return (
+    <Box
+      flex={flex}
+      p={8}
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      mt={4}
+      mx={4}
+      bg='teal'
+      color='white'
+    >
+      {dataMenu?.data.values.map((item) => (
+        <div key={item.total_order}>
+          <Text fontSize="3xl" as="b" ml={4}>
+            This Month
+          </Text>
+          <Stack mt={6}>
+            <Table>
+              <Tr>
+                <Th color='white'>Total Orders</Th>
+                <Td isNumeric>{item.total_order} Orders</Td>
+              </Tr>
+              <Tr>
+                <Th color='white'>Income</Th>
+                <Td isNumeric>{item.total_income} Orders</Td>
+              </Tr>
+            </Table>
+          </Stack>
+        </div>
+      ))}
+    </Box>
+  );
+}
--- src/pages/admin/orders/pending.jsx
+++ src/pages/admin/orders/pending.jsx
@@ -9,7 +9,6 @@ import {
 } from "@chakra-ui/react";
 
 
-
 export default function Orders() {
     return (
         <>
